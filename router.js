39b02f321ccc94d46f4e977857855d23097ec8c8
abrar shams chowdhury <abrar.chowdhury99@gmail.com>
2021-01-11 11:27:50 +0600

    first commit before github

--- router.js
+++ router.js
@@ -1,8 +1,76 @@
 const express = require("express");
 const router = express.Router();
+const Patient = require("./models/patient")
 
-router.get("/", (req, res) => {
-  res.send({ response: "Server is up and running." }).status(200);
-});
+
+//____ Create ______
+router.post("/patient", (req,res) => {
+  const {name, age, sex, bed} = req.body
+  console.log("new user", name, bed, age, sex)
+  const patient = new Patient({ bed, name, age, sex })
+  patient.save((err)=>{
+        if(err){
+            console.log(err)
+        }else{
+            console.log("new patient admitted")
+        }
+    })
+})
+
+//____ Read All______
+router.get("/patient", (req,res)=>{
+  Patient.find( function (err, patients) {
+    if(err){
+        console.log(err)
+    }else{
+        res.json(patients)
+    }
+  })
+})
+
+//____ Read ______
+router.get("/patient/:bed", (req,res)=>{
+  const bed = parseInt(req.params.bed)
+  Patient.findOne({ bed: bed }, function (err, patient){
+    if(err){
+        console.log(err)
+    }else{
+        res.json(patient)
+    }
+  })
+})
+
+
+
+//____ Update ______
+router.put("/patient/:bed", (req,res)=>{
+  const {name, age, sex} = req.body
+  const bed = parseInt(req.params.bed)
+  
+  console.log(bed, name, age, sex)
+  Patient.findOneAndUpdate({ bed:bed }, {$set:{ name, age, sex }},function(err, doc){
+    if(err){
+        console.log("Something wrong when updating data!");
+    }
+    console.log(doc);
+  });
+
+})
+
+
+//____ Delete ______
+router.delete("/patient/:bed", (req,res)=>{
+  console.log("delete route hit")
+  console.log(req.body.bed)
+  const bed = req.body.bed
+  Patient.remove({ bed: bed}, (err)=>{
+    if(err){
+      console.log(err)
+    }else{
+      console.log("successfully deleted it")
+    }
+  });
+})
+
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,29 +1,64 @@
 const http = require('http');
 const express = require('express');
+const bodyParser  = require("body-parser")
+const mongoose = require('mongoose')
 const socketio = require('socket.io')
-const cors = require('cors');
+
+
+const cors = require('cors')
+
+//db
+const dbUri = "mongodb+srv://abrar:abrar123@cluster0.gzrt5.mongodb.net/vsm?retryWrites=true&w=majority"
+mongoose.connect(dbUri, {useNewUrlParser: true, useUnifiedTopology: true}, function(err){
+    if(err){
+        console.log('Mongo Connection Error: ',err);
+    }else{
+        console.log('Mongo Connection established')
+    }
+});
 
 const router = require('./router');
 
 let count=0
-
 const app = express();
+app.use(cors())
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json())
 const server = http.createServer(app);
 const io = socketio(server);
 
+let espCount = "nothing yet"
+let spo2 = "nothing yet" 
+let pulse ="hardware-fix, Naming convention"
+let temp = "nothing yet" 
+let ecg = "nothing yet" 
 
+
+// SOCKET IO 
 io.on('connection', ( socket ) => {
     console.log('new connection')
-    socket.on('vsm',({inputValue})=>{
+    // console.log(socket)
+
+    socket.on('vsm',({ inputValue })=>{
         console.log("ping", inputValue)
+        
     })
-
-    setInterval(()=>{
-        socket.emit('live', {liveData: count})
-        count++ 
-    },3000)
-
+    socket.on('message',(message)=>{
+        console.log('_____________________________________')
+        console.log("ESP-Data ->")
+        console.log(message);
+        console.log('_____________________________________')
+        //sends to all clients except sender
+        socket.broadcast.emit('message', message)
+    })
+    // setInterval(()=>{
+    //     socket.emit('live', { count ,espCount, spo2, pulse, temp, ecg })
+    //     count++ 
+    // },3000)
+    socket.emit('vsm',"hello esp");
+    
     socket.on('disconnect',()=>{
+        console.log('____________DISCONNECTED_________________________')
         console.log('user had left')
     })
 
